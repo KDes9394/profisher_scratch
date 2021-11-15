ea89109353adc8f3700b4f0ff0e4cd16d9f144f4
lasj <kentousa@gmail.com>
2021-11-14 18:16:59 -0800

    pip installed flask-login

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,6 +11,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Job(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,5 +24,8 @@ class Job(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+
     def __repr__(self):
         return '<Job {}>'.format(self.body)
+
+
