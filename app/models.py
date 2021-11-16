e159d03b32b6845c146074afa630c1deb80b902e
lasj <kentousa@gmail.com>
2021-11-15 17:48:01 -0800

    Fixed ImportError

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from app import login, login_manager
+from app import login
 from datetime import datetime
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -10,6 +10,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     jobs = db.relationship('Job', backref='author', lazy='dynamic')
+    # employer = db.Column(db.String(120), index=True, unique=True)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -25,6 +26,8 @@ class Job(db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    title = db.column(db.String(140))
+
 
 
     def __repr__(self):
@@ -32,9 +35,6 @@ class Job(db.Model):
 
 
 @login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
-@login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
