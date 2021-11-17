5eb2ce6221f2ee213efc5470e83cf5581414a7e2
lasj <kentousa@gmail.com>
2021-11-16 19:10:32 -0800

    added profile

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from app import login
 from datetime import datetime
 from app import db
@@ -11,6 +12,12 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     jobs = db.relationship('Job', backref='author', lazy='dynamic')
     # employer = db.Column(db.String(120), index=True, unique=True)
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+        digest, size)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -20,6 +27,7 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
 
 class Job(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,6 +40,17 @@ class Job(db.Model):
 
     def __repr__(self):
         return '<Job {}>'.format(self.body)
+
+class Application(db.Model):
+    application_id = db.Column(db.Serial, primary_key=True)
+    job_id =  db.Column(db.Integer, db.ForeignKey('job.id'))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    email_address = db.Column(db.String(50))
+    city_location = db.Column(db.String(50))
+
+
 
 
 @login.user_loader
